e7ba20b998b93d48a346933a697ab35a628e3454
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2023-04-28 09:51:45 -0600

    added return test

--- src/lib/LibBurn.sol
+++ src/lib/LibBurn.sol
@@ -30,24 +30,24 @@ library LibBurn {
     /// @notice Implementation should not different from eth function.
     /// @param _amount Amount of ETH to burn.
     function test_eth(uint256 _amount) internal returns (address contract_addr) {
-    assembly ("memory-safe") {
-        // Store creation code of no-op contract
-        // PUSH1 0xFE (Invalid opcode)
-        // PUSH1 0x00  \
-        // MSTORE       > Replace with PUSH0 / CHAINID for mainnet.
-        // PUSH1 0x01  /
-        // PUSH1 0x1F
-        // RETURN
-        mstore(0x00, 0x60fe6000526001601ff3)
-        contract_addr := create(_amount, 0x16, 0x0a)
+        assembly ("memory-safe") {
+            // Store creation code of no-op contract
+            // PUSH1 0xFE (Invalid opcode)
+            // PUSH1 0x00  \
+            // MSTORE       > Replace with PUSH0 / CHAINID for mainnet.
+            // PUSH1 0x01  /
+            // PUSH1 0x1F
+            // RETURN
+            mstore(0x00, 0x60fe6000526001601ff3)
+            contract_addr := create(_amount, 0x16, 0x0a)
 
-        if iszero(contract_addr) {
-            // "BurnFailed()" error
-            mstore(0x00, 0x6f16aafc)
-            revert(0x1c, 0x04)
+            if iszero(contract_addr) {
+                // "BurnFailed()" error
+                mstore(0x00, 0x6f16aafc)
+                revert(0x1c, 0x04)
+            }
         }
     }
-}
 
     /// Burns a given amount of gas.
     /// @param _amount Amount of gas to burn.
